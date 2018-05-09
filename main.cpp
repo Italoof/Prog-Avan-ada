6b3852ba3a6799f585dc5784717ce108cdd5b919
Italoof <italo.of@hotmail.com>
2018-05-09 08:39:48 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,32 +1,9 @@
 #include <iostream>
-#include "point.h"
-#include "poligono.h"
-#include "retangulo.h"
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-
-    Retangulo ret(0,0,4,3);
-
-    ret.imprime();
-
-    cout<<"\nÁrea =" <<ret.area()<<endl<<endl;
-
-    ret.translada(-3,4);
-
-    ret.imprime();
-
-    cout<<"\nÁrea depois de transladado=" <<ret.area()<<endl<<endl;
-
-    ret.rot(30,-1,2.5);
-
-    ret.imprime();
-
-    cout<<"\nÁrea depois de transladado e rotacionado=" <<ret.area()<<endl<<endl;
-
+    cout << "Hello World!" << endl;
     return 0;
-
 }
-
---
+++ screen.cpp
@@ -0,0 +1,44 @@
+#include "screen.h"
+#include <vector>
+
+
+using namespace std;
+
+Screen::Screen(int nlin_, int ncol_)
+{
+    nlin=nlin_;
+    ncol=ncol_;
+    mat= vector< vector<char> > (nlin_, vector<char>(ncol_,' '));
+    brush='#';
+}
+
+void Screen::setPixel(int x, int y)
+{
+    mat[x][y]=brush;
+}
+
+void Screen::clear()
+{
+    mat= vector< vector<char> > (nlin, vector<char>(ncol,' '));
+}
+
+void Screen::setBrush(char brush_)
+{
+    brush=brush_;
+}
+
+ostream &operator<<(ostream &os, Screen &t)
+{
+    for(int i=0;i<t.nlin;i++){
+        for(int j=0; j<t.ncol; j++){
+            os<< t.mat[i][j];
+        }
+    }
+
+    return os;
+}
+
+
+
+
+
